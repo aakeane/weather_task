a6455b560c817acc35b81d69e05bf1dcc34c9f05
aakeane <aaronjkeane@gmail.com>
2022-09-19 10:10:12 +0100

    Define web routes, create empty controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('welcome');});
+Route::post('/', [WeatherController::class, 'getWeather']);
+
+Route::fallback(function () {return redirect('/');});
